81388dc2644797738b469076117b17633b22c74a
Geethma Nimsarani <99175974+geethmaN@users.noreply.github.com>
2024-02-01 11:27:34 +0530

    Update auth.js
    delete

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,14 +5,5 @@ module.exports = (req, res, next) => {
     if(!token){
         return res.status(401).json({msg: "No Token - Authorization Denied"})
     }
-    try{
-        const verified = jwt.verify(token, process.env.JWT_SECRET)
-        if(!verified){
-            return res.status(401).json({msg: "Cannot Verify - Authorization Denied"})
-        }
-        req.user = verified
-        next()
-    } catch(err){
-        res.status(401).json({msg: err})
-    }
-}
+
+    
